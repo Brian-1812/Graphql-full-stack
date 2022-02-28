3cdb785d3ed0c395aeb310cf7ca8fa9b015b43cc
Shokhrukhmirzo Toyirov <toirovshohruxmirzo@gmail.com>
2022-02-28 19:08:59 +0500

    SSR configured

--- lireddit-client/src/components/Navbar.tsx
+++ lireddit-client/src/components/Navbar.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
   const [, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(),
+  });
+
   let body = null;
   if (fetching) {
   } else if (!data?.me) {
---
+++ lireddit-client/src/utils/createUrqlClient.ts
@@ -0,0 +1,68 @@
+import { dedupExchange, fetchExchange } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
+import {
+  LoginMutation,
+  LogoutMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
+import { udpateQuery } from "./udpateQuery";
+
+export const createUrqlClient = (_ssrExchange: any, ctx: any) => ({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include" as const,
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            udpateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            udpateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+          logout: (_result, args, cache, info) => {
+            udpateQuery<LogoutMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              () => ({ me: null })
+            );
+          },
+        },
+      },
+    }),
+    _ssrExchange,
+    fetchExchange,
+  ],
+});
